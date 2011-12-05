bc6b73426aeaddd120406ab1e614940a16398fec
Bryan <bryan@bryanhelmig.com>
2011-12-04 21:17:53 -0600

    setuptools, distribute setup.py

--- setup.py
+++ setup.py
@@ -1,67 +1,18 @@
-from distutils.core import setup
-import sys
+from setuptools import setup, find_packages
 
-pkgdir = {'': 'python%s' % sys.version_info[0]}
 VERSION = '0.7.2'
 
-setup(name='httplib2',
-        version=VERSION, 
-        author='Joe Gregorio',
-        author_email='joe@bitworking.org',
-        url='https://github.com/bryanhelmig/httplib2-bigcerts/',
-        download_url='https://github.com/bryanhelmig/httplib2-bigcerts/',
-        description='A comprehensive HTTP client library.',
-        license='MIT',
-        long_description="""
-
-A comprehensive HTTP client library, ``httplib2`` supports many features left out of other HTTP libraries.
-
-**HTTP and HTTPS**
-  HTTPS support is only available if the socket module was compiled with SSL support. 
- 
-
-**Keep-Alive**
-  Supports HTTP 1.1 Keep-Alive, keeping the socket open and performing multiple requests over the same connection if possible. 
-
-
-**Authentication**
-  The following three types of HTTP Authentication are supported. These can be used over both HTTP and HTTPS.
-
-  * Digest
-  * Basic
-  * WSSE
-
-**Caching**
-  The module can optionally operate with a private cache that understands the Cache-Control: 
-  header and uses both the ETag and Last-Modified cache validators. Both file system
-  and memcached based caches are supported.
-
-
-**All Methods**
-  The module can handle any HTTP request method, not just GET and POST.
-
-
-**Redirects**
-  Automatically follows 3XX redirects on GETs.
-
-
-**Compression**
-  Handles both 'deflate' and 'gzip' types of compression.
-
-
-**Lost update support**
-  Automatically adds back ETags into PUT requests to resources we have already cached. This implements Section 3.2 of Detecting the Lost Update Problem Using Unreserved Checkout
-
-
-**Unit Tested**
-  A large and growing set of unit tests.
-
-http://httplib2.googlecode.com/svn/trunk/#egg=httplib2-dev
-        """,
-        package_dir=pkgdir,
-        packages=['httplib2'],
-        package_data={'httplib2': ['*.txt']},
-        classifiers=[
+setup(
+    name='httplib2',
+    version=VERSION, 
+    author='Joe Gregorio',
+    author_email='joe@bitworking.org',
+    url='https://github.com/bryanhelmig/httplib2-bigcerts/',
+    download_url='https://github.com/bryanhelmig/httplib2-bigcerts/',
+    description='A comprehensive HTTP client library.',
+    license='MIT',
+    packages = find_packages(),
+    classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
@@ -71,5 +22,8 @@ http://httplib2.googlecode.com/svn/trunk/#egg=httplib2-dev
         'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries',
-        ],
-        )
+    ],
+    keywords = 'httplib2 http certs',
+    zip_safe = True,
+    install_requires=['distribute']
+)
