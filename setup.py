59c9fcf9057dcc8f5ac5818155d7343f4f21714f
Bryan Helmig <bryan@bryanhelmig.com>
2011-12-30 22:09:35 -0600

    python2 only and trying to get pip to freeze correctly

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 from setuptools import setup, find_packages
 import sys
 
-pkgdir = {'': 'python%s' % sys.version_info[0]}
 VERSION = '0.7.2'
 
-setup(name="httplib2",
-      version=VERSION,
-      description='A comprehensive HTTP client library.',
-      license="MIT License",
-      author='Joe Gregorio',
-      author_email='joe@bitworking.org',
-      url="http://github.com/bryanhelmig/httplib2-bigcerts",
-      package_dir=pkgdir,
-      packages = find_packages(),
-      install_requires = ['distribute'],
-      keywords= "httplib2 http certs",
+setup(name = 'httplib2',
+      version = VERSION,
+      description = 'A comprehensive HTTP client library.',
+      license = 'MIT License',
+      author = 'Joe Gregorio',
+      author_email = 'joe@bitworking.org',
+      url = 'http://github.com/bryanhelmig/httplib2',
+      package_dir = pkgdir,
+      keywords = 'httplib2 http certs'
 )
