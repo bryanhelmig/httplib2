8637bc8038f9827915eb13131eb5d5c1abe7fcce
Bryan Helmig <bryan@bryanhelmig.com>
2011-12-30 21:51:10 -0600

    trying to fix that silly pip issue

--- setup.py
+++ setup.py
@@ -4,29 +4,15 @@ import sys
 pkgdir = {'': 'python%s' % sys.version_info[0]}
 VERSION = '0.7.2'
 
-setup(
-    name='httplib2',
-    version=VERSION, 
-    author='Joe Gregorio',
-    author_email='joe@bitworking.org',
-    url='https://github.com/bryanhelmig/httplib2-bigcerts/',
-    download_url='https://github.com/bryanhelmig/httplib2-bigcerts/',
-    description='A comprehensive HTTP client library.',
-    license='MIT',
-    package_dir=pkgdir,
-    packages=['httplib2'],
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries',
-    ],
-    keywords = 'httplib2 http certs',
-    zip_safe = True,
-    install_requires=['distribute']
+setup(name="httplib2",
+      version=VERSION,
+      description='A comprehensive HTTP client library.',
+      license="MIT License",
+      author='Joe Gregorio',
+      author_email='joe@bitworking.org',
+      url="http://github.com/bryanhelmig/httplib2-bigcerts",
+      package_dir=pkgdir,
+      packages = find_packages(),
+      install_requires = ['distribute'],
+      keywords= "httplib2 http certs",
 )
